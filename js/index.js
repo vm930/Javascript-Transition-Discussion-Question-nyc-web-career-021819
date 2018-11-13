b8f4e1a8a38e2e631a1db7d4d99006a6841c5df2
maxwellbenton <maxwellbenton@gmail.com>
2018-11-13 13:09:20 -0500

    updated solution

--- js/index.js
+++ js/index.js
@@ -7,22 +7,22 @@
 //   $('#foreground').fadeOut(interval);
 // }
 
-const saveInterval = document.getElementById('interval')
-const startSaving = document.getElementById('save_lincoln').addEventListener("click", ()=>(saveLincoln(parseInt(saveInterval.value))))
-const zombieFace = document.getElementById('foreground')
-const hiddenMessage = document.getElementById('hidden-message')
+const saveInterval = document.getElementById( 'interval' )
+const startSaving = document.getElementById( 'save_lincoln' ).addEventListener( "click", () => ( saveLincoln( parseInt( saveInterval.value ) ) ) )
+const zombieFace = document.getElementById( 'foreground' )
+const hiddenMessage = document.getElementById( 'hidden-message' )
 
-function saveLincoln(interval) {
-  if(isNaN(interval)) {
+function saveLincoln( interval ) {
+  if ( isNaN( interval ) ) {
     interval = 100
   }
   var op = 1;
-  var timer = setInterval(function () {
-      if (op <= 0.01){
-          clearInterval(timer);
-          zombieFace.style.display = 'none';
-      }
-      zombieFace.style.opacity = op;
-      op -= op * 0.01;
-  }, interval);
+  var timer = setInterval( function () {
+    if ( op <= 0.01 ) {
+      clearInterval( timer );
+      zombieFace.style.display = 'none';
+    }
+    zombieFace.style.opacity = op;
+    op -= op * 0.01;
+  }, interval );
 }
